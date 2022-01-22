bd0127721d076215a7c180810991bf0ca4ea5f5e
Vartika <bhardwajvartika9450@gmail.com>
2022-01-23 05:19:59 +0530

    First commmit

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,15 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  return <main>
+    <section className='container'>
+      <h3>0 birthday today</h3>
+      <list/>
+      <button onClick={()=> console.log("you clicked me")}>Clear All</button>
+
+    </section>
+
+  </main>
 }
 
 export default App;
